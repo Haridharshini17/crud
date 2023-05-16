768baf101a6c2db35348de307db8994bd6273d2e
Haridharshini17 <haridha.senthil@aspiresys.com>
2023-05-16 14:33:08 +0530

    Added changes in form

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,17 +6,18 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('name')
             ->add('email')
             ->add('password')
-            ->add('created_at')
-            ->add('updated_at')
-        ;
+            ->getForm()
+            ->add('Save', SubmitType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
